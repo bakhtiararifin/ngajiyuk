4ce8de9708f47a66405b99aa9e64ab8a4ffc64fd
Bakhtiar Arifin <bakhtiar.arifin.88@gmail.com>
2021-02-27 16:21:59 +0700

    home page

--- lib/common/features/dashboard/widgets/home_page.dart
+++ lib/common/features/dashboard/widgets/home_page.dart
@@ -10,9 +10,43 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
+        title: Text('Ngajiyuk'),
       ),
-      body: Container(),
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: EdgeInsets.all(16),
+        mainAxisSpacing: 16,
+        crossAxisSpacing: 16,
+        children: <Widget>[
+          _Lesson('Sholat'),
+          _Lesson('Sholat'),
+          _Lesson('Sholat'),
+          _Lesson('Sholat'),
+        ],
+      ),
+    );
+  }
+}
+
+class _Lesson extends StatelessWidget {
+  final String title;
+  const _Lesson(
+    this.title, {
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: InkWell(
+        onTap: () {},
+        child: Center(
+          child: Text(
+            title,
+            style: TextStyle(fontSize: 24.0),
+          ),
+        ),
+      ),
     );
   }
 }

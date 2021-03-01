5071ab262ee63f2166b7793f6d76d3bce8901bf7
Bakhtiar Arifin <bakhtiar.arifin.88@gmail.com>
2021-03-01 23:08:35 +0700

    setup sentry

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'dart:async';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:ngajiyuk/core/services/configure_injection.dart';
 import 'package:ngajiyuk/core/widgets/app_widget.dart';
 import 'package:ngajiyuk/core/widgets/global_bloc_provider.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,15 +15,19 @@ Future<void> main() async {
   await initializeDateFormatting('id_ID', null);
   await Firebase.initializeApp();
   await configureInjection();
+  await DotEnv.load(fileName: ".env");
 
   return runZonedGuarded(() async {
+    await SentryFlutter.init((options) {
+      options.dsn = DotEnv.env['SENTRY_DSN'];
+    });
+
     runApp(
       GlobalBlocProvider(
         child: AppWidget(),
       ),
     );
   }, (error, stack) {
-    print(stack);
-    print(error);
+    Sentry.captureException(error, stackTrace: stack);
   });
 }

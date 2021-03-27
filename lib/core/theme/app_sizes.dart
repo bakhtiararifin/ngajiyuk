76afd6fd2662923e7dcfd1ad502610b4a63d32a7
Bakhtiar Arifin <bakhtiar.arifin.88@gmail.com>
2021-03-27 23:25:17 +0700

    app sizes

---
+++ lib/core/theme/app_sizes.dart
@@ -0,0 +1,7 @@
+class AppSizes {
+  static const paddingExtraLarge = 40.0;
+  static const paddingLarge = 32.0;
+  static const paddingMedium = 24.0;
+  static const paddingRegular = 16.0;
+  static const paddingSmall = 8.0;
+}

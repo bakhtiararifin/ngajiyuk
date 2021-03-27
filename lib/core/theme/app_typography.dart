94f45e14a485663bbc7d9b9227e29f4c0ab64858
Bakhtiar Arifin <bakhtiar.arifin.88@gmail.com>
2021-03-27 23:25:29 +0700

    app typography

---
+++ lib/core/theme/app_typography.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/widgets.dart';
+
+class AppTypography {
+  static const h1 = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 40,
+  );
+
+  static const h2 = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 32,
+  );
+
+  static const h3 = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 24,
+  );
+
+  static const body = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 16,
+  );
+
+  static const small = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 12,
+  );
+}

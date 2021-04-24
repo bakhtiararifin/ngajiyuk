3e8b64f36d91e006b75072b53f1cfefb96367262
Bakhtiar Arifin <bakhtiar.arifin.88@gmail.com>
2021-04-25 05:58:34 +0700

    payment info dialog

--- lib/lesson/features/lesson/lesson_info.dart
+++ lib/lesson/features/lesson/lesson_info.dart
@@ -55,8 +55,14 @@ class _LessonPricing extends StatelessWidget {
       return Column(
         children: [
           SizedBox(height: 8),
-          Text(
-              'Dalam proses verifikasi pembayaran. Segera lakukan pembayaran jika kamu belum melakukan.'),
+          SizedBox(
+            width: double.infinity,
+            child: OutlinedButton.icon(
+              icon: Icon(Icons.info),
+              label: Text('Menunggu pembayaran'),
+              onPressed: () => _showPaymentInfoDialog(context),
+            ),
+          ),
         ],
       );
     }
@@ -77,6 +83,38 @@ class _LessonPricing extends StatelessWidget {
           ),
         ),
       ],
+    );
+  }
+
+  void _showPaymentInfoDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Menunggu Pembayaran'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                _ConfirmationDescription(lesson: lesson),
+                SizedBox(height: 16),
+                _HowToPay(lesson: lesson),
+                SizedBox(height: 16),
+                Text(
+                  'Setelah itu kami akan mengkonfirmasi pembayaran kamu dan kamu bisa tonton video pembelajaran yang sudah kamu pilih.',
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 
@@ -103,5 +141,105 @@ class _LessonPricing extends StatelessWidget {
         (route) => route.isFirst,
       );
     }
+  }
+}
+
+class _HowToPay extends StatelessWidget {
+  const _HowToPay({
+    Key? key,
+    required this.lesson,
+  }) : super(key: key);
+
+  final Lesson lesson;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('1. '),
+            Expanded(
+              child: RichText(
+                text: TextSpan(
+                  text: 'Transfer pembayaran sebesar ',
+                  style: DefaultTextStyle.of(context).style,
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: Formatter.formatNumber(lesson.sellPrice),
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    TextSpan(
+                      text: ' ke rekening ',
+                    ),
+                    TextSpan(
+                      text:
+                          'Bank Syariah Mandiri nomor 12345678 a/n Bakhtiar Arifin',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    TextSpan(
+                      text: ' dan simpan bukti transfer nya.',
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: 16),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('2. '),
+            Expanded(
+              child: RichText(
+                text: TextSpan(
+                  text:
+                      'Kirim bukti transfer, nama user, dan judul pelajaran via whatsapp ke nomor ',
+                  style: DefaultTextStyle.of(context).style,
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: '08123456789',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class _ConfirmationDescription extends StatelessWidget {
+  const _ConfirmationDescription({
+    Key? key,
+    required this.lesson,
+  }) : super(key: key);
+
+  final Lesson lesson;
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+        text: 'Sedikit lagi kamu bisa menonton video ',
+        style: DefaultTextStyle.of(context).style,
+        children: <TextSpan>[
+          TextSpan(
+            text: lesson.title,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          TextSpan(
+            text: '. Kamu tinggal melakukan langkah langkah dibawah ini:',
+          ),
+        ],
+      ),
+    );
   }
 }

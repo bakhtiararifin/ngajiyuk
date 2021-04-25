50b7bff30388485857874bfa102e9cc73b4e8194
Bakhtiar Arifin <bakhtiar.arifin.88@gmail.com>
2021-04-25 07:36:52 +0700

    lesson bloc listen to lessons bloc

--- lib/lesson/blocs/lesson/lesson_bloc.dart
+++ lib/lesson/blocs/lesson/lesson_bloc.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
+import 'package:ngajiyuk/lesson/blocs/lessons/lessons_bloc.dart';
 import 'package:ngajiyuk/lesson/model/lesson/lesson.dart';
 
 part 'lesson_event.dart';
@@ -11,7 +12,11 @@ part 'lesson_bloc.freezed.dart';
 
 @lazySingleton
 class LessonBloc extends Bloc<LessonEvent, LessonState> {
-  LessonBloc() : super(_Initial());
+  final LessonsBloc _lessonsBloc;
+
+  StreamSubscription? _lessonsListener;
+
+  LessonBloc(this._lessonsBloc) : super(_Initial()) {}
 
   @override
   Stream<LessonState> mapEventToState(
@@ -23,6 +28,31 @@ class LessonBloc extends Bloc<LessonEvent, LessonState> {
   }
 
   Stream<LessonState> _setLesson(Lesson lesson) async* {
+    _lessonsListener?.cancel();
+
+    _lessonsListener = _lessonsBloc.stream.listen((event) {
+      final List<Lesson>? lessons = event.maybeWhen(
+        success: (List<Lesson> lessons) => lessons,
+        orElse: () => null,
+      );
+
+      if (lessons == null) return;
+
+      final Iterable<Lesson> updatedLessons = lessons.where(
+        (element) => element.id == lesson.id,
+      );
+
+      if (updatedLessons.length > 0) {
+        add(LessonEvent.setLesson(updatedLessons.first));
+      }
+    });
+
     yield LessonState.success(lesson);
+  }
+
+  @override
+  Future<void> close() {
+    _lessonsListener?.cancel();
+    return super.close();
   }
 }
